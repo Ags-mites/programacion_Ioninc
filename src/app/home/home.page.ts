c2a4957cda2885fe63a3f468db137e35c41f3b95
ags-mites <eamites1@espe.edu.ec>
2024-07-27 14:29:56 -0500

    Requerimiento 2 completado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonMenu, IonButtons, IonButton, IonMenuButton } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonMenu, IonButtons, IonButton, IonMenuButton],
 })
 export class HomePage {
+  counters: { [key: string]: number } = {
+    banana: 0,
+    apple: 0,
+    pear: 0,
+    grape: 0,
+    lemon: 0
+  };
+
   constructor() {}
+
+  increment(fruit: string) {
+    if (this.counters.hasOwnProperty(fruit)) {
+      this.counters[fruit]++;
+    }
+  }
+
+  reset() {
+    this.counters = {
+      banana: 0,
+      apple: 0,
+      pear: 0,
+      grape: 0,
+      lemon: 0
+    };
+  }
 }
